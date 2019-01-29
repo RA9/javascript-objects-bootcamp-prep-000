045581cf4f97780eee728508379cd9da4a5728c8
Carlos Nah <cnah27@gmail.com>
2019-01-29 18:22:38 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ const playlist = {"Psquare": "Do me"}
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[playlist] = { artistName : songTitle }
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+}
